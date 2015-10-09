0c38f9b4cfff088ea1574703b9f41c6882f78a95
Zach Waugh <zwaugh@gmail.com>
2015-10-09 10:19:02 -0400

    Add iPhone 6s, 6s Plus, iPad mini 4, and iPod touch 6th gen

--- ZWDevice.m
+++ ZWDevice.m
@@ -31,7 +31,7 @@
 {
     struct utsname systemInfo;
     uname(&systemInfo);
-    
+
     return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
 }
 
@@ -55,6 +55,8 @@
                     @"iPhone6,2": @"iPhone 5s",
                     @"iPhone7,1": @"iPhone 6 Plus",
                     @"iPhone7,2": @"iPhone 6",
+                    @"iPhone8,1": @"iPhone 6s",
+                    @"iPhone8,2": @"iPhone 6s Plus",
                     @"iPad1,1": @"iPad",
                     @"iPad2,1": @"iPad 2",
                     @"iPad2,2": @"iPad 2",
@@ -63,30 +65,33 @@
                     @"iPad2,5": @"iPad mini",
                     @"iPad2,6": @"iPad mini",
                     @"iPad2,7": @"iPad mini",
-                    @"iPad3,1": @"iPad (3rd gen)",
-                    @"iPad3,2": @"iPad (3rd gen)",
-                    @"iPad3,3": @"iPad (3rd gen)",
-                    @"iPad3,4": @"iPad (4th gen)",
-                    @"iPad3,5": @"iPad (4th gen)",
-                    @"iPad3,6": @"iPad (4th gen)",
+                    @"iPad3,1": @"iPad 3",
+                    @"iPad3,2": @"iPad 3",
+                    @"iPad3,3": @"iPad 3",
+                    @"iPad3,4": @"iPad 4",
+                    @"iPad3,5": @"iPad 4",
+                    @"iPad3,6": @"iPad 4",
                     @"iPad4,1": @"iPad Air",
                     @"iPad4,2": @"iPad Air",
-                    @"iPad4,4": @"iPad mini (2nd gen)",
-                    @"iPad4,5": @"iPad mini (2nd gen)",
-                    @"iPad4,6": @"iPad mini (2nd gen)",
-                    @"iPad4,7": @"iPad mini (3rd gen)",
-                    @"iPad4,8": @"iPad mini (3rd gen)",
-                    @"iPad4,9": @"iPad mini (3rd gen)",
+                    @"iPad4,4": @"iPad mini 2",
+                    @"iPad4,5": @"iPad mini 2",
+                    @"iPad4,6": @"iPad mini 2",
+                    @"iPad4,7": @"iPad mini 3",
+                    @"iPad4,8": @"iPad mini 3",
+                    @"iPad4,9": @"iPad mini 3",
+                    @"iPad5,1": @"iPad mini 4",
+                    @"iPad5,2": @"iPad mini 4",
                     @"iPad5,3": @"iPad Air 2",
                     @"iPad5,4": @"iPad Air 2",
                     @"iPod1,1": @"iPod touch",
                     @"iPod2,1": @"iPod touch (2nd gen)",
                     @"iPod3,1": @"iPod touch (3rd gen)",
                     @"iPod4,1": @"iPod touch (4th gen)",
-                    @"iPod5,1": @"iPod touch (5th gen)"
+                    @"iPod5,1": @"iPod touch (5th gen)",
+                    @"iPod7,1": @"iPod touch (6th gen)"
                     };
     });
-    
+
     NSString *hardwareModel = [self hardwareModel];
     return _models[hardwareModel] ?: UIDevice.currentDevice.model;
 }
